ef63458f6833f30fcd0dfb74252032935efd30da
wlsgur828 <51524368+wlsgur828@users.noreply.github.com>
2019-06-11 23:41:47 +0900

    Update Dachore.py

--- Dachore.py
+++ Dachore.py
@@ -4,12 +4,20 @@ import os
 os.system("clear")
 
 # banner
-f=open("banner.txt",'r')
+f = open("banner.txt",'r')
 data=f.read()
 print(data)
 f.close()
 
-imageName = input ('plz imageName = ')
-url = 'http://13.209.35.206:8000/DachoreApp/'+imageName
-res = requests.get(url)
-print (res.text)
+while(True):
+  imageName = input ('plz imageName = ')
+  url = 'http://13.209.35.206:8000/DachoreApp/'+imageName
+  print ("\n        ---------- {} Analyzing ----------\n".format(imageName))
+  res = requests.get(url)
+  
+  if res.status_code == 404:
+    print ("\n"+res.text+"\n")
+    continue
+  elif res.status_code == 200:
+    print (res.text)
+    break
